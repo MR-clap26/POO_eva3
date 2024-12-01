439eee1fdeaa860c164f26557a483e350aa216b3
Diego Bravo <diego.art.bra@gmail.com>
2024-12-01 01:03:40 -0300

    clase to do y archivo de consmo de api creado

---
+++ modelos/todo.py
@@ -0,0 +1,9 @@
+class Todo:
+    def __init__(self,userId=0,id=0,title='',completed='') :
+        self.userId=userId
+        self.id=id
+        self.title=title
+        self.completed=completed
+
+
+        
